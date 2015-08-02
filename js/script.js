434d7ecea47b1c0616565dd13772df62f9a56c75
KevinGao2201 <kevingao.2201@gmail.com>
2015-08-01 23:36:20 -0400

    inprove rule of tiles coming up

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function autoHover() {
     var wrapper = document.getElementsByClassName("wrapper"),
         n = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
-    wrapper[n].classList.toggle( "hover" );
+    wrapper[n].classList.add( "hover" );
+    setTimeout(function(){wrapper[n].classList.remove( "hover")},2000);
 }
 window.setInterval(autoHover, 2000);
