93276a3e0883f333bb8055736f0fed1db08d907d
KevinGao2201 <kevingao.2201@gmail.com>
2015-08-02 18:34:39 -0400

    go next button works now

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,40 @@
-$(document).ready(function() {
-    var wrapper = document.getElementsByClassName("wrapper");
-    $(wrapper).click(function open() {
-        $("header").toggleClass("activate");
-        $("hungry").toggleClass("activate");
-        $("main").toggleClass("activate");
-        $(".wrapper").not(this).removeClass("activate");
-        $(".wrapper").not(this).toggleClass("hidden");
-        $(this).removeClass("hidden");
-        $(this).removeClass("hover");
-        $(this).toggleClass("activate");
-    });
-});
+var main = function() {
+    var wrapper = $(".wrapper");
+    var opened = $(".current");
+    var closed = $(".wrapper").not(".current");
+    var toggleTitle = function() {
+            $("header").toggleClass("activate");
+            $("hungry").toggleClass("activate");
+            $("main").toggleClass("activate");
+        };
+    var openClose = function() {
+            $(".wrapper").not(this).removeClass("current");
+            $(".wrapper").not(this).toggleClass("hidden");
+            $(this).removeClass("hidden");
+            $(this).removeClass("hover");
+            $(this).toggleClass("current");
+        };
+//    var close = function() {
+//            $(".wrapper").not(this).removeClass("hidden");
+//            $(this).removeClass("current");
+//        };
+    $(wrapper).click(toggleTitle);
+    $(wrapper).click(openClose);
+//    $(closed).click(open);
+    
+    var next = function() {
+        var currentSlide = $(".current");
+        var nextSlide = currentSlide.next();
+        if (nextSlide.length == 0) {
+            nextSlide = $(".wrapper").first();
+        };
+        currentSlide.removeClass("current");
+        $(wrapper).not(currentSlide).removeClass("hidden");
+        nextSlide.addClass("current");
+        $(wrapper).not(nextSlide).addClass("hidden");
+    };
+    $("#next").click(next);
+};
+                       
+
+$(document).ready(main);
