1eb85e99e6a5bf8f3371e3146f2e85fd936ea8d6
KevinGao2201 <kevingao.2201@gmail.com>
2015-08-02 19:32:13 -0400

    add prev btn, apply style to buttons, buttons hidden on load

--- js/jquery.js
+++ js/jquery.js
@@ -3,17 +3,18 @@ var main = function() {
     var opened = $(".current");
     var closed = $(".wrapper").not(".current");
     var toggleTitle = function() {
-            $("header").toggleClass("activate");
-            $("hungry").toggleClass("activate");
-            $("main").toggleClass("activate");
-        };
+        $("header").toggleClass("activate");
+        $("hungry").toggleClass("activate");
+        $("main").toggleClass("activate");
+        $(".buttons").toggleClass("hidden");
+    };
     var openClose = function() {
-            $(".wrapper").not(this).removeClass("current");
-            $(".wrapper").not(this).toggleClass("hidden");
-            $(this).removeClass("hidden");
-            $(this).removeClass("hover");
-            $(this).toggleClass("current");
-        };
+        $(".wrapper").not(this).removeClass("current");
+        $(".wrapper").not(this).toggleClass("hidden");
+        $(this).removeClass("hidden");
+        $(this).removeClass("hover");
+        $(this).toggleClass("current");
+    };
 //    var close = function() {
 //            $(".wrapper").not(this).removeClass("hidden");
 //            $(this).removeClass("current");
@@ -34,6 +35,19 @@ var main = function() {
         $(wrapper).not(nextSlide).addClass("hidden");
     };
     $("#next").click(next);
+    
+    var previous = function() {
+        var currentSlide = $(".current");
+        var prevSlide = currentSlide.prev();
+        if (prevSlide.length == 0) {
+            prevSlide = $(".wrapper").last();
+        };
+        currentSlide.removeClass("current");
+        $(wrapper).not(currentSlide).removeClass("hidden");
+        prevSlide.addClass("current");
+        $(wrapper).not(prevSlide).addClass("hidden");
+    };
+    $("#prev").click(previous);
 };
                        
 
