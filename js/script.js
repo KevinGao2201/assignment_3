f9915782ed99e7ec7c221e116407b85c87513310
KevinGao2201 <kevingao.2201@gmail.com>
2015-08-02 03:59:33 -0400

    add close button for calendar,js allows toggling info on and off

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
 function autoHover() {
     var wrapper = document.getElementsByClassName("wrapper"),
-        n = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+        n = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
     wrapper[n].classList.add( "hover" );
-    setTimeout(function(){wrapper[n].classList.remove( "hover")},2000);
+    setTimeout(function(){wrapper[n].classList.remove("hover")},2000);
 }
 window.setInterval(autoHover, 2000);
+
+function removeCalen() {
+        var calendar = document.getElementById("calendar");
+        calendar.classList.add("hidden");
+}
+
+window.onload = function() {
+    var wrapper = document.getElementsByClassName("wrapper");
+    for (var i = 0; i < wrapper.length; i++)
+        wrapper[i].onclick = function open() {
+        var header = document.querySelector("header"),
+            hungry = document.getElementById("hungry_div"),
+            main = document.querySelector("main"),
+            activeTile = this;
+//            nonActiveTile = document.getElementById("no_"+i);
+        header.classList.toggle("activate");
+        hungry.classList.toggle("activate");
+        main.classList.toggle("activate");
+        activeTile.classList.toggle("activate");
+        nonActiveTile.classList.toggle("hidden");
+        nonActiveTile.classList.remove("activate");
+    }
+}
+
+//window.onload = function() {
+//    document.getElementById("no_2").onclick = function fun() {
+//        var header = document.querySelector("header"),
+//            hungry = document.getElementById("hungry_div"),
+//            main = document.querySelector("main"),
+//            activeTile = document.getElementById("no_2"),
+//            nonActiveTile = document.getElementById("no_1");
+////        alert("hello");
+//        header.classList.add("activate");
+//        hungry.classList.add("activate");
+//        main.classList.add("activate");
+//        activeTile.classList.add("activate");
+//        nonActiveTile.classList.add("hidden");
+//    }
+//}
